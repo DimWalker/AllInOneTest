364c5e8d6d310271784d74686fc50242ebb9f904
DimWalker <dimwalker@qq.com>
2014-10-01 13:30:24 +0800

    AllInOneTest
    AllInOneTest

---
+++ Common/Http/HttpWebHelper.cs
@@ -0,0 +1,119 @@
+﻿/*
+图片下载参考地址：http://blog.csdn.net/vip__888/article/details/5646260
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Common.Http
+{
+    public sealed class HttpWebHelper
+    {
+        public HttpWebHelper() { }
+
+        #region 图片下载
+        /// <summary>
+        /// 下载图片
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="savePath"></param>
+        /// <returns></returns>
+        public static bool DowloadImg(string Url, string savePath)
+        {
+            return DowloadImg(Url, savePath);
+        }
+        /// <summary>
+        /// 下载图片，传参cookCon，可以验证码
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="savePath"></param>
+        /// <param name="cookCon"></param>
+        /// <returns></returns>
+        public static bool DowloadImg(string Url, string savePath, CookieContainer cookCon)
+        {
+            bool bol = true;
+
+            try
+            {
+
+                File.WriteAllBytes(savePath, GetImgHex(Url, cookCon));
+
+            }
+            catch (IOException ex)
+            {
+                bol = false;
+            }
+            catch (Exception ex)
+            {
+                bol = false;
+            }
+            return bol;
+        }
+
+        /// <summary>
+        /// 获取图片二进制数组
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <returns></returns>
+        public static byte[] GetImgHex(string Url)
+        {
+            return GetImgHex(Url, new CookieContainer());
+        }
+        /// <summary>
+        /// 获取图片二进制数组
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="cookCon"></param>
+        /// <returns></returns>
+        public static byte[] GetImgHex(string Url, CookieContainer cookCon)
+        {
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(Url);
+            //属性配置
+            webRequest.AllowWriteStreamBuffering = true;
+            webRequest.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            webRequest.MaximumResponseHeadersLength = -1;
+            webRequest.Accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*";
+            webRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; Maxthon; .NET CLR 1.1.4322)";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Method = "GET";
+            webRequest.Headers.Add("Accept-Language", "zh-cn");
+            webRequest.Headers.Add("Accept-Encoding", "gzip,deflate");
+            webRequest.KeepAlive = true;
+            webRequest.CookieContainer = cookCon;
+            try
+            {
+                //获取服务器返回的资源
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    using (Stream sream = webResponse.GetResponseStream())
+                    {
+                        List<byte> list = new List<byte>();
+                        while (true)
+                        {
+                            int data = sream.ReadByte();
+                            if (data == -1)
+                            { break; }
+                            list.Add((byte)data);
+                        }
+                        return list.ToArray();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+    }
+}
---
+++ HeartStone/GrabCard.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GrabCard.aspx.cs" Inherits="HeartStone.GrabCard" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <script type="text/javascript" src="Javascript/jquery-1.7.2.min.js"></script>
+
+    <script type="text/javascript">
+        function loadPage() {
+            //            $("#divFrame").load("http://db.duowan.com/lushi/card/list/eyJwIjoxLCJzb3J0IjoiaWQuZGVzYyJ9.html");
+
+            $.ajax({
+            url: "http://img.dwstatic.com/ls/pic/card/Unstable%20Ghoul.png",
+                cache: false,
+                success: function(html) {
+                    $("#divFrame").append(html);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    // 通常 textStatus 和 errorThrown 之中
+                    // 只有一个会包含信息
+                    alert(this); // 调用本次AJAX请求时传递的options参数
+                }
+            });
+
+            $("#cardimg").attr("src","http://img.dwstatic.com/ls/pic/card/Unstable%20Ghoul.png");
+        }
+    
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <input type="button" id="loadFrame" name="loadFrame" onclick="loadPage();" value="loadFrame" />
+    <div id="divFrame" style="border-style: solid; border-width: 3px; border-color: Black;
+        height: 100px; width:100px;">
+    </div>
+    <img id="cardimg" width="100" height="100"/>
+    
+    <iframe src="http://db.duowan.com/lushi/card/list/eyJwIjoxLCJzb3J0IjoiaWQuZGVzYyJ9.html" height="800" width="100%"></iframe>
+    </form>
+</body>
+</html>
